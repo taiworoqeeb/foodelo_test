b23fc3d28a9251cc334108f969669504585c36c9
Raqeeb Taiwo <taiworoqeeb@gmail.com>
2022-06-27 15:18:00 +0100

    -a

--- model/Carts.js
+++ model/Carts.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const CartSchema = mongoose.Schema({
     user:{
         type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
+        ref: "Users"
     },
    cartItem:[{
         type: mongoose.Schema.Types.ObjectId,
